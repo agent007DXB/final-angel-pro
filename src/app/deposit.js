989657b8dda95013acb85ae622be15f517828a41
Agentkuldeep <cyberjunkeez.org@gmail.com>
2024-07-18 02:04:48 +0530

    init

--- src/app/deposit.js
+++ src/app/deposit.js
@@ -1,15 +1,14 @@
-import React, {useRef} from 'react'
-import './globals.css'
+import React, { useRef, useState } from 'react';
+import './globals.css';
 import { X, Copy } from 'lucide-react';
 import Image from 'next/image';
 import QR from "@/app/QR.jpeg";
-import { useState } from 'react';
 
-function deposit({onClose}) {
-    const ModalRef = useRef();
+const Deposit = ({ onClose }) => {
+    const modalRef = useRef();
 
     const closeModal = (e) => {
-        if(ModalRef.current === e.target){
+        if (modalRef.current === e.target) {
             onClose();
         }
     }
@@ -18,69 +17,67 @@ function deposit({onClose}) {
     const [email, setEmail] = useState("");
     const [txid, setTxid] = useState("");
     const [amount, setAmount] = useState("");
+    const [copyText, setCopyText] = useState('TXii93QZLLpFEWpz32WES1bJiWQDjnrD14');
 
-    const [copyText, setCopytext] = useState('')
-    const handleCopy=()=>{
+    const handleCopy = () => {
         navigator.clipboard.writeText(copyText)
-        alert('copied')
+        alert('Copied');
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
-        if (!name || !email || !txid || !amount ) {
-          alert("All the fields are required.");
-          return;
+
+        if (!name || !email || !txid || !amount) {
+            alert("All the fields are required.");
+            return;
         }
-    
+
         try {
-          const res = await fetch("http://localhost:3000/api/deposit", {
-            method: "POST",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify({name, email, txid, amount}),
-          });
-    
-          if (res.ok) {
-            router.push("/");
-          } else {
-            throw new Error("Failed");
-          }
+            const res = await fetch("http://localhost:3000/api/deposit", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify({ name, email, txid, amount }),
+            });
+
+            if (res.ok) {
+                router.push("/");
+            } else {
+                throw new Error("Failed");
+            }
         } catch (error) {
-          console.log(error);
+            console.log(error);
         }
-      };
-    
+    };
 
-
-  return (
-    <div ref={ModalRef} onClick={closeModal} className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
-        <div className='mt-10 flex flex-col gap-5 text-white'>
-            <button onClick={onClose} className='place-self-end'>
-                <X size={30}/>
-            </button>
-            <div className='bg-orange-600 rounded-xl px-20 py-10 flex flex-col gap-3 items-center mx-4'>
-                <h1 className='text-3x1 font-bold text-white'>DEPOSIT YOUR FUNDS</h1>
-                <h3 className='font-bold text-black'>Scan this QR to pay</h3>
-                <Image src={QR} alt="my-image" width={100} height={100}/>
-                <p className='font-bold max-w-md text-center'>If have transaction fee, don't forget to add it. The transfer amount must match the deposit amount</p>
-                <form onSubmit={handleSubmit}>
-                    <input className='w-full px-4 py-3 text-black border-black rounded-md' placeholder='Enter your name' name='name' id='name' value={name} onChange={(e) => setName(e.target.value)} required></input>
-                    <br/><br/>
-                    <input className='w-full px-4 py-3 text-black border-black rounded-md' placeholder='Enter your email' name='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)} required></input>
-                    <br/><br/>
-                    <input className='w-full px-4 py-3 text-black border-black rounded-md' placeholder='Enter your txid' name='txid' id='txid' value={txid} onChange={(e) => setTxid(e.target.value)} required></input>
-                    <br/><br/>
-                    <input className='w-full px-4 py-3 text-black border-black rounded-md' placeholder='Enter the amount' name='amount' id='amount' value={amount} onChange={(e) => setAmount(e.target.value)} required></input>
-                    <br/><br/>
-                    <p value={copyText} onChange={(e) => setCopytext(e.target.value)}>TXii93QZLLpFEWpz32WES1bJiWQDjnrD14 <button onClick={handleCopy}><Copy /></button> </p>
-                    <button type='submit' className='mt-4 w-full flex item-center justify-center gap-2 px-5 py-3 font-medium rounded-md bg-black' onClick={closeModal}>Submit</button>
-                </form>
+    return (
+        <div ref={modalRef} onClick={closeModal} className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center'>
+            <div className='mt-10 flex flex-col gap-5 text-white'>
+                <button onClick={onClose} className='place-self-end'>
+                    <X size={30} />
+                </button>
+                <div className='bg-orange-600 rounded-xl px-20 py-10 flex flex-col gap-3 items-center mx-4'>
+                    <h1 className='text-3x1 font-bold text-white'>DEPOSIT YOUR FUNDS</h1>
+                    <h3 className='font-bold text-black'>Scan this QR to pay</h3>
+                    <Image src={QR} alt="QR Code" width={100} height={100} />
+                    <p className='font-bold max-w-md text-center'>If there is a transaction fee, ensure it is included. The transfer amount must match the deposit amount.</p>
+                    <form onSubmit={handleSubmit}>
+                        <input className='w-full px-4 py-3 text-black border-black rounded-md' placeholder='Enter your name' name='name' id='name' value={name} onChange={(e) => setName(e.target.value)} required></input>
+                        <br /><br />
+                        <input className='w-full px-4 py-3 text-black border-black rounded-md' placeholder='Enter your email' name='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)} required></input>
+                        <br /><br />
+                        <input className='w-full px-4 py-3 text-black border-black rounded-md' placeholder='Enter your txid' name='txid' id='txid' value={txid} onChange={(e) => setTxid(e.target.value)} required></input>
+                        <br /><br />
+                        <input className='w-full px-4 py-3 text-black border-black rounded-md' placeholder='Enter the amount' name='amount' id='amount' value={amount} onChange={(e) => setAmount(e.target.value)} required></input>
+                        <br /><br />
+                        <p>{copyText} <button onClick={handleCopy}><Copy /></button></p>
+                        <button type='submit' className='mt-4 w-full flex item-center justify-center gap-2 px-5 py-3 font-medium rounded-md bg-black' onClick={closeModal}>Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default deposit
+export default Deposit;
