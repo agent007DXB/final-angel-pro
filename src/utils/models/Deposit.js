c70de1bee45bbb1bca4a9ce4d27bc963a2f2db47
agentkuldeep <cyberjunkeez.org@gmail.com>
2024-07-18 17:17:41 +0530

    Update Deposit.js

--- src/utils/models/Deposit.js
+++ src/utils/models/Deposit.js
@@ -1,30 +1,29 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-const DepositSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-        },
-        txid: {
-            type: String,
-            required: true,
-        },
-        amount: {
-            type: String,
-            required: true,
-        },
-        
+const DepositSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamp: true,
-    }
+    email: {
+      type: String,
+      required: true,
+    },
+    txid: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Corrected from 'timestamp' to 'timestamps'
+  }
 );
 
-const Deposit = mongoose.model.Deposit || mongoose.model("Deposit", DepositSchema);
+const Deposit = mongoose.models.Deposit || mongoose.model("Deposit", DepositSchema);
 
 export default Deposit;
