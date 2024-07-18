07d18a0890a484ccfbae2794112e591fcd504ba9
agentkuldeep <cyberjunkeez.org@gmail.com>
2024-07-18 17:05:59 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Authprovider from '@/component/Authprovider/Authprovider'
+import Authprovider from "@/components/Authprovider/Authprovider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Authprovider>
-        {children}
-      </Authprovider>
-        </body>
+        <Authprovider>{children}</Authprovider>
+      </body>
     </html>
   );
 }
