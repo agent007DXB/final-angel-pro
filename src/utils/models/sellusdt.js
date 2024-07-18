30fa26208ecfc2429a235448fbc52bab47806348
agentkuldeep <cyberjunkeez.org@gmail.com>
2024-07-18 17:18:12 +0530

    Update sellusdt.js

--- src/utils/models/sellusdt.js
+++ src/utils/models/sellusdt.js
@@ -1,38 +1,37 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-const sellusdtSchema = new Schema(
-    {
-        usdtVol: {
-            type: String,
-            required: true,
-        },
-        phone: {
-            type: String,
-            required: true,
-        },
-        txref: {
-            type: String,
-            required: true,
-        },
-        accnum: {
-            type: String,
-            required: true,
-        },
-        accifsc: {
-            type: String,
-            required: true,
-        },
-        accname: {
-            type: String,
-            required: true,
-        },
-        
+const sellusdtSchema = new mongoose.Schema(
+  {
+    usdtVol: {
+      type: String,
+      required: true,
     },
-    {
-        timestamp: true,
-    }
+    phone: {
+      type: String,
+      required: true,
+    },
+    txref: {
+      type: String,
+      required: true,
+    },
+    accnum: {
+      type: String,
+      required: true,
+    },
+    accifsc: {
+      type: String,
+      required: true,
+    },
+    accname: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // Corrected to enable timestamps
+  }
 );
 
-const Sellusdt = mongoose.model.Sellusdt || mongoose.model("SellUsdt", sellusdtSchema);
+const Sellusdt = mongoose.models.Sellusdt || mongoose.model("SellUsdt", sellusdtSchema);
 
 export default Sellusdt;
